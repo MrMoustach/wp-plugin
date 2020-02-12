b95a47bac02e0d1cd2f3d6ef0e8a0bc01002b365
MrMoustach <issam.harchi123@gmail.com>
2020-02-12 23:21:39 +0100

    first commit

---
+++ example-plugin.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Simplton
+Plugin URI: 
+Description: A simple example of a plugin for ista where u can add a page to admin panel
+Author: Sherlock
+Author URI: http://google.com/
+Version: 1.0.0
+*/
+defined('ABSPATH') or die('This is not meant to be used in scripts !!');
+add_action("admin_menu", "addMenu");
+function addMenu()
+{
+  add_menu_page("Example Options", "Example Options", 4, "example-options", "exampleMenu" );
+  add_submenu_page("example_options", "Option 1", "Option 1", 4, "example-option-1", "option1");
+}
+
+function exampleMenu()
+{
+/*echo <<< 'EOD'
+
+  <h2> This is a simple text where u can</h2>
+
+
+EOD;*/
+include('test.php');
+}
+
+function option1()
+{
+  echo "Walrus";
+}
---
+++ test.php
@@ -0,0 +1,3 @@
+<div class="test">
+    <h1>This is a test heading to test the page</h1>
+</div>
