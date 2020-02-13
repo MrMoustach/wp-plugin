7e012ba1d7d3a2b9b2ffe4a8f64887c03c01f4f9
MrMoustach <issam.harchi123@gmail.com>
2020-02-13 18:19:42 +0100

    third commit

--- test.php
+++ test.php
@@ -1,3 +1,16 @@
 <div class="test">
     <h1>This is a test heading to test the page</h1>
 </div>
+<?= get_option('siteurl',false); ?>
+<form action="#" method="POST">
+    <input type="text" name="url" value="<?= get_option('blogname',false) ?>">
+    <input type="submit" name="submit" value="Change">
+</form>
+<?php
+    if(isset($_POST['submit']))
+    {
+        if(!empty($_POST['url'])){
+            update_option('blogname',$_POST['url'],null);
+        }
+    }
+?>
